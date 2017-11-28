1c49fb7fd83204d03a9cd5ad9e0453317c84bf35
Marcus Guthrie <marcusguthrie111@gmail.com>
2017-11-28 20:25:50 +0000

    Add styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
-import numeral from 'numeral';
+
 
 export const ExpensesSummary = (props) => {
     const expenseString = props.expensesCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('0,0.00')
     return (
-        <div>
-            <h2>Viewing {props.expensesCount} {expenseString} totalling £{formattedExpensesTotal}</h2>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expensesCount}</span> {expenseString} totalling <span>£{formattedExpensesTotal}</span>
+                </h1>
+                <div className="page-header__actions">  
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>     
     );
 };  
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 // Link and NavLink (from React Router api) dont do a full page refresh when navigating to a new page, taking advantage of React
 // NavLink is good for navigation menus, can style the font weight, color etc
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <button onClick={startLogout}>Logout</button>
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Expensify</h1>
+                </Link>
+                <button className="button button--link" onClick={startLogout}>Logout</button>
+            </div>
+        </div>
     </header>
 );
 
